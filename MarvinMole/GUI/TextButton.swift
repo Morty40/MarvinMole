f76b081b17f5206dae4cb9d48b974dbb4235294c
Morty40 <morten@perriard.dk>
2026-05-06 17:49:01 +0200

    TextButton, work in progress

---
+++ MarvinMole/GUI/TextButton.swift
@@ -0,0 +1,32 @@
+//
+//  TextButton.swift
+//  MarvinMole
+//
+//  (c) Morten Perriard 2026
+//
+
+import SpriteKit
+
+class TextButton: Button {
+        
+    private lazy var titleLabel = {
+        let node = SKLabelNode()
+        node.fontColor = .white
+        node.position = .zero
+        node.horizontalAlignmentMode = .center
+        node.verticalAlignmentMode = .center
+        node.zPosition = 2
+        return node
+    }()
+
+    init(title: String, target: Any?, action: Selector?) {
+        super.init(target: target, action: action)
+    
+        titleLabel.text = title
+        addChild(titleLabel)
+    }
+    
+    @MainActor required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
