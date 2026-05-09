698d71abae80e61875e7d93f35aa8dffe3384414
Morty40 <morten@perriard.dk>
2026-05-09 16:11:22 +0200

    hero move, game scene

--- MarvinMole/Map.swift
+++ MarvinMole/Map.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// Sokoban map representation
-struct Map {
+class Map {
 
     enum StaticTile {
         case none
@@ -47,11 +47,212 @@ struct Map {
         }
     }
 
+    // Sokoban solution LURD format
+    enum Move: String {
+        case walkLeft  = "l"
+        case walkUp    = "u"
+        case walkRight = "r"
+        case walkDown  = "d"
+        case pushLeft  = "L"
+        case pushUp    = "U"
+        case pushRight = "R"
+        case pushDown  = "D"
+        
+        var isPush: Bool {
+            [.pushLeft, .pushRight, .pushUp, .pushDown].contains(self)
+        }
+    }
+
     private var staticTiles: [[StaticTile]] = []
     private var objectTiles: [[ObjectTile]] = []
+    private var completedMoves: [Move] = []
 
+    init(staticTiles: [[StaticTile]], objectTiles: [[ObjectTile]]) {
+        self.staticTiles = staticTiles
+        self.objectTiles = objectTiles
+        self.completedMoves = []
+    }
+    
+    /// Map size
     var size: (width: Int, height: Int) {
         (staticTiles.map(\.count).max() ?? 0, staticTiles.count)
+    }
+        
+    var numberOfMoves: Int {
+        completedMoves.count
+    }
+
+    var numberOfPushes: Int {
+        completedMoves.count(where: \.isPush)
+    }
+
+    /// Returns the number of occurences of a specific type of tile
+    /// - Parameter tile: Tile type
+    /// - Returns: Number of occurences in map
+    func count(of tile: StaticTile) -> Int {
+        staticTiles.joined().count(where: { $0 == tile })
+    }
+
+    /// Returns the number of occurences of a specific type of tile
+    /// - Parameter tile: Tile type
+    /// - Returns: Number of occurences in map
+    func count(of tile: ObjectTile) -> Int {
+        objectTiles.joined().count(where: { $0 == tile })
+    }
+
+    /// Returns the static tile at coordinate (x, y)
+    /// - Parameters:
+    ///   - x: X-coordinate
+    ///   - y: Y-coordinate
+    /// - Returns: Tile, or .none if outside the map
+    func staticTileAt(x: Int, y: Int) -> StaticTile {
+        if y >= 0, y < staticTiles.count {
+            let row = staticTiles[y]
+            if x >= 0, x < row.count {
+                return row[x]
+            }
+        }
+        return .none
+    }
+
+    /// Returns the object tile at coordinate (x, y)
+    /// - Parameters:
+    ///   - x: X-coordinate
+    ///   - y: Y-coordinate
+    /// - Returns: Tile, or .none if outside the map
+    func objectTileAt(x: Int, y: Int) -> ObjectTile {
+        if y >= 0, y < objectTiles.count {
+            let row = objectTiles[y]
+            if x >= 0, x < row.count {
+                return row[x]
+            }
+        }
+        return .none
+    }
+
+    func putObjectTileAt(x: Int, y: Int, _ tile: ObjectTile) {
+        objectTiles[y][x] = tile
+    }
+    
+    var heroPosition: (x: Int, y: Int)? {
+        if let heroY = objectTiles.firstIndex(where: { $0.contains(.hero) } ),
+           let heroX = objectTiles[heroY].firstIndex(of: .hero) {
+            return (heroX, heroY)
+        }
+        return nil
+    }
+ 
+    var boxPositions: [(x: Int, y: Int)] {
+        var positions: [(x: Int, y: Int)] = []
+        
+        for (y, row) in objectTiles.enumerated() {
+            for (x, tile) in row.enumerated() where tile == .box {
+                positions.append((x, y))
+            }
+        }
+        
+        return positions
+    }
+    
+    var legalMoves: [Move] {
+        var moves: [Move] = []
+        
+        if let (heroX, heroY) = heroPosition {
+            
+            let staticTile = staticTileAt(x: heroX, y: heroY)
+            let staticTileU = staticTileAt(x: heroX, y: heroY-1)
+            let staticTileD = staticTileAt(x: heroX, y: heroY+1)
+            let staticTileL = staticTileAt(x: heroX-1, y: heroY)
+            let staticTileR = staticTileAt(x: heroX+1, y: heroY)
+            let staticTileUU = staticTileAt(x: heroX, y: heroY-2)
+            let staticTileDD = staticTileAt(x: heroX, y: heroY+2)
+            let staticTileLL = staticTileAt(x: heroX-2, y: heroY)
+            let staticTileRR = staticTileAt(x: heroX+2, y: heroY)
+
+            // TODO: also check for objects...
+            
+            if staticTileU != .wall {
+                moves.append(.walkUp)
+            }
+            if staticTileD != .wall {
+                moves.append(.walkDown)
+            }
+            if staticTileL != .wall {
+                moves.append(.walkLeft)
+            }
+            if staticTileR != .wall {
+                moves.append(.walkRight)
+            }
+        }
+        
+        return moves
+    }
+    
+    private func moveObjectTile(from: (x: Int, y: Int), to: (x: Int, y: Int)) {
+        let objectTile = objectTileAt(x: from.x, y: from.y)
+        putObjectTileAt(x: from.x, y: from.y, .none)
+        putObjectTileAt(x: to.x, y: to.y, objectTile)
+    }
+
+    @discardableResult
+    func doNextMove(_ move: Move) -> Bool {
+        if legalMoves.contains(move), let heroPosition = heroPosition {
+
+            let heroX = heroPosition.x
+            let heroY = heroPosition.y
+            
+            switch move {
+            case .walkLeft:
+                moveObjectTile(from: heroPosition, to: (heroX-1, heroY))
+            case .walkUp:
+                moveObjectTile(from: heroPosition, to: (heroX, heroY-1))
+            case .walkRight:
+                moveObjectTile(from: heroPosition, to: (heroX+1, heroY))
+            case .walkDown:
+                moveObjectTile(from: heroPosition, to: (heroX, heroY+1))
+            case .pushLeft:
+                moveObjectTile(from: (heroX-1, heroY), to: (heroX-2, heroY))
+                moveObjectTile(from: heroPosition, to: (heroX-1, heroY))
+            case .pushUp:
+                moveObjectTile(from: (heroX, heroY-1), to: (heroX, heroY-2))
+                moveObjectTile(from: heroPosition, to: (heroX, heroY-1))
+            case .pushRight:
+                moveObjectTile(from: (heroX+1, heroY), to: (heroX+2, heroY))
+                moveObjectTile(from: heroPosition, to: (heroX+1, heroY))
+            case .pushDown:
+                moveObjectTile(from: (heroX, heroY+1), to: (heroX, heroY+2))
+                moveObjectTile(from: heroPosition, to: (heroX, heroY+1))
+            }
+
+            completedMoves.append(move)
+            return true
+        }
+        return false
+    }
+    
+    @discardableResult
+    func undoLastMove() -> Bool {
+        if let lastMove = completedMoves.popLast() {
+            
+            return true
+        }
+        return false
+    }
+    
+    /// Check if all goals have a box on top
+    var isCompleted: Bool {
+        for (y, row) in staticTiles.enumerated() {
+            for (x, tile) in row.enumerated() where tile == .goal {
+                
+                if objectTileAt(x: x, y: y) != .box {
+                    // no box on top of this goal, the map is not completed
+                    return false
+                }
+            }
+        }
+        
+        // all goals have a box on top, the map is completed
+        return true
     }
     
     static func mapFromXsb(data: Data) -> Map? {
@@ -76,9 +277,15 @@ struct Map {
             ObjectTile.tileFrom(xsb: String($0))
         })})
         
-        // find coordinate of hero
-        let heroY = objectTiles.firstIndex(where: { $0.contains(.hero) } )!
-        let heroX = objectTiles[heroY].firstIndex(of: .hero)!
+        // find the hero coordinate
+        var heroX: Int? = nil
+        var heroY: Int? = nil
+        if let y = objectTiles.firstIndex(where: { $0.contains(.hero) } ) {
+            if let x = objectTiles[y].firstIndex(of: .hero) {
+                heroX = x
+                heroY = y
+            }
+        }
 
         func fillFloors(_ tiles: inout [[StaticTile]], x: Int, y: Int) {
             if y >= 0, y < tiles.count {
@@ -94,9 +301,11 @@ struct Map {
             }
         }
         
-        // recursive fill floors inside walls
+        // recursive fill floors inside walls starting from hero
         // (xsb data unfortunately has floors both inside and outside the walls)
-        fillFloors(&staticTiles, x: heroX, y: heroY)
+        if let heroX = heroX, let heroY = heroY {
+            fillFloors(&staticTiles, x: heroX, y: heroY)
+        }
         
         let staticTiles2: [[StaticTile]] = lines.map({ $0.map({
             StaticTile.tileFrom(xsb: String($0))
@@ -112,38 +321,4 @@ struct Map {
         return Map(staticTiles: staticTiles, objectTiles: objectTiles)
     }
 
-    
-    /// Returns the number of occurences of a specific type of tile
-    /// - Parameter tile: Tile type
-    /// - Returns: Number of occurences in map
-    func count(of tile: StaticTile) -> Int {
-        staticTiles.joined().count(where: { $0 == tile })
-    }
-
-    /// Returns the number of occurences of a specific type of tile
-    /// - Parameter tile: Tile type
-    /// - Returns: Number of occurences in map
-    func count(of tile: ObjectTile) -> Int {
-        objectTiles.joined().count(where: { $0 == tile })
-    }
-
-    /// Returns the tile at coordinate (x, y)
-    /// - Parameters:
-    ///   - x: X-coordinate
-    ///   - y: Y-coordinate
-    /// - Returns: Tile, or .none if outside the map
-    func staticTileAt(x: Int, y: Int) -> StaticTile {
-        if y >= 0, y < staticTiles.count {
-            let row = staticTiles[y]
-            if x >= 0, x < row.count {
-                return row[x]
-            }
-        }
-        return .none
-    }
-    
-    func objectsOf(type: ObjectTile) -> [(x: Int, y: Int)] {
-        return [] // TODO: fix me
-    }
-    
 }
--- MarvinMole/Scenes/GameScene.swift
+++ MarvinMole/Scenes/GameScene.swift
@@ -7,6 +7,35 @@
 
 import SpriteKit
 
+class GameObjectNode: SKSpriteNode {
+    
+    var mapPosition: (x: Int, y: Int) = (0, 0) {
+        didSet {
+            let w = parent?.frame.width ?? 0
+            let h = parent?.frame.height ?? 0
+
+            position = CGPoint(x: 32*(CGFloat(mapPosition.x)+0.5) - w/2,
+                               y: h/2 - 32*(CGFloat(mapPosition.y)+0.5))
+        }
+    }
+        
+    init() {
+        let texture = SKTexture(imageNamed: "Hero")
+        super.init(texture: texture, color: .red, size: texture.size())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class Box: GameObjectNode {
+}
+
+class Hero: GameObjectNode {
+    
+}
+
 private let smallXsb = """
     #####
     #   #
@@ -23,6 +52,15 @@ private let smallXsb = """
 
 class GameScene: Scene {
     
+    private var map: Map? = nil
+    private var boxes: [Box] = []
+        
+    enum Input {
+        case left, up, right, down, undo
+    }
+    private var pendingInput: [Input] = []
+    private var isMoving: Bool = false
+    
     private lazy var backgroundImage = {
         let node = SKSpriteNode(imageNamed: "GameBackground")
         node.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
@@ -31,20 +69,70 @@ class GameScene: Scene {
         return node
     }()
 
-    private lazy var staticTileMap = {
-        let node = StaticTileMapNode()
-        node.position = CGPoint(x: frame.size.width * 0.43, y: frame.size.height * 0.5)
+    private lazy var levelLabel = {
+        let node = SKLabelNode()
+        node.fontName = "Avenir-Black"
+        node.position = CGPoint(x: frame.size.width * 0.86, y: frame.size.height * 0.73)
+        node.fontColor = .blue
+        node.horizontalAlignmentMode = .center
+        node.verticalAlignmentMode = .center
         node.zPosition = 2
-        
-        let smallMap = Map.mapFromXsb(string: smallXsb)!
-        node.draw(map: smallMap)
-        
+        node.text = "Easy 1"
         return node
     }()
 
+    private lazy var pushesLabel = {
+        let node = SKLabelNode()
+        node.fontName = "Avenir-Black"
+        node.position = CGPoint(x: frame.size.width * 0.86, y: frame.size.height * 0.62)
+        node.fontColor = .blue
+        node.horizontalAlignmentMode = .center
+        node.verticalAlignmentMode = .center
+        node.zPosition = 2
+        return node
+    }()
+
+    private lazy var movesLabel = {
+        let node = SKLabelNode()
+        node.fontName = "Avenir-Black"
+        node.position = CGPoint(x: frame.size.width * 0.86, y: frame.size.height * 0.50)
+        node.fontColor = .blue
+        node.horizontalAlignmentMode = .center
+        node.verticalAlignmentMode = .center
+        node.zPosition = 2
+        return node
+    }()
+    
+    private lazy var staticTileMap = {
+        let node = StaticTileMapNode()
+        node.position = CGPoint(x: frame.size.width * 0.43, y: frame.size.height * 0.5)
+        node.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        node.zPosition = 2
+        return node
+    }()
+
+    private lazy var hero = {
+        let node = Hero()
+        node.position = .zero
+        node.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        node.zPosition = 3
+        return node
+    }()
+
+    private lazy var undoButton = {
+        let node = TextButton(title: "Undo", target: self, action: #selector(onUndo))
+        node.position = CGPoint(x: frame.size.width * 0.8, y: frame.size.height * 0.1)
+        node.zPosition = 2
+        return node
+    }()
+    
+    @objc func onUndo() {
+        pendingInput.append(.undo)
+    }
+
     private lazy var quitButton = {
         let node = TextButton(title: "Quit", target: self, action: #selector(onQuit))
-        node.position = CGPoint(x: frame.size.width * 0.8, y: frame.size.height * 0.1)
+        node.position = CGPoint(x: frame.size.width * 0.2, y: frame.size.height * 0.1)
         node.zPosition = 2
         return node
     }()
@@ -56,55 +144,127 @@ class GameScene: Scene {
     override func sceneDidLoad() {
         super.sceneDidLoad()
         print("sceneDidLoad: GameScene")
-
-        // setup gesture recognizers for four way navigation
-        view?.addGestureRecognizer(
-            UISwipeGestureRecognizer(target: self,
-                                     action: #selector(onSwipeUp),
-                                     direction: .up))
-        view?.addGestureRecognizer(
-            UISwipeGestureRecognizer(target: self,
-                                     action: #selector(onSwipeDown),
-                                     direction: .down))
-        view?.addGestureRecognizer(
-            UISwipeGestureRecognizer(target: self,
-                                     action: #selector(onSwipeLeft),
-                                     direction: .left))
-        view?.addGestureRecognizer(
-            UISwipeGestureRecognizer(target: self,
-                                     action: #selector(onSwipeRight),
-                                     direction: .right))
         
         anchorPoint = .zero
         
         addChild(backgroundImage)
+        addChild(levelLabel)
+        addChild(pushesLabel)
+        addChild(movesLabel)
         addChild(staticTileMap)
+        addChild(undoButton)
         addChild(quitButton)
-        
+
+        staticTileMap.addChild(hero)
     }
     
     override func didMove(to view: SKView) {
         print("didMoveTo: GameScene")
-    }
-    
-    @objc func onSwipeUp() {
-        print("swipe up")
-    }
-    
-    @objc func onSwipeDown() {
-        print("swipe down")
+        
+        // remove any old gesture recognizers
+        for recognizer in view.gestureRecognizers ?? [] {
+            view.removeGestureRecognizer(recognizer)
+        }
+        
+        // add gesture recognizers for four way navigation
+        view.addGestureRecognizer(
+            UISwipeGestureRecognizer(target: self,
+                                     action: #selector(onSwipeLeft),
+                                     direction: .left))
+        view.addGestureRecognizer(
+            UISwipeGestureRecognizer(target: self,
+                                     action: #selector(onSwipeUp),
+                                     direction: .up))
+        view.addGestureRecognizer(
+            UISwipeGestureRecognizer(target: self,
+                                     action: #selector(onSwipeRight),
+                                     direction: .right))
+        view.addGestureRecognizer(
+            UISwipeGestureRecognizer(target: self,
+                                     action: #selector(onSwipeDown),
+                                     direction: .down))
+        
+        // load map
+        map = Map.mapFromXsb(string: smallXsb)!
+        
+        staticTileMap.draw(map: map!)
+        
+        if let heroPosition = map!.heroPosition {
+            //hero.position = CGPoint(x: CGFloat(heroPosition.x) + 0.5, y: CGFloat(heroPosition.y) + 0.5)
+            hero.mapPosition = heroPosition
+            //addChild(hero!)
+        }
+        
+        for boxPosition in map!.boxPositions {
+            let box = Box()
+            box.mapPosition = boxPosition
+            boxes.append(box)
+        }
+        
     }
     
     @objc func onSwipeLeft() {
-        print("swipe left")
+        pendingInput.append(.left)
     }
-    
+
+    @objc func onSwipeUp() {
+        pendingInput.append(.up)
+    }
+
     @objc func onSwipeRight() {
-        print("swipe right")
+        pendingInput.append(.right)
+    }
+
+    @objc func onSwipeDown() {
+        pendingInput.append(.down)
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        
+        pushesLabel.text = "\(map!.numberOfPushes)"
+        movesLabel.text = "\(map!.numberOfMoves)"
+
+        processInput()
     }
     
-    override func update(_ currentTime: TimeInterval) {
+    private func processInput() {
+        if !isMoving, let nextInput = pendingInput.first {
+            
+            let legalMoves = map!.legalMoves
+            
+            switch nextInput {
+            case .left:
+                if legalMoves.contains(.walkLeft) {
+                    map!.doNextMove(.walkLeft)
+                } else if legalMoves.contains(.pushLeft) {
+                    map!.doNextMove(.pushLeft)
+                }
+            case .up:
+                if legalMoves.contains(.walkUp) {
+                    map!.doNextMove(.walkUp)
+                } else if legalMoves.contains(.pushUp) {
+                    map!.doNextMove(.pushUp)
+                }
+            case .right:
+                if legalMoves.contains(.walkRight) {
+                    map!.doNextMove(.walkRight)
+                } else if legalMoves.contains(.pushRight) {
+                    map!.doNextMove(.pushRight)
+                }
+            case .down:
+                if legalMoves.contains(.walkDown) {
+                    map!.doNextMove(.walkDown)
+                } else if legalMoves.contains(.pushDown) {
+                    map!.doNextMove(.pushDown)
+                }
+            case .undo:
+                map!.undoLastMove()
+            }
+            
+            pendingInput.removeFirst()
+        }
     }
+    
 }
 
 // sokoban tileset used for testing:
