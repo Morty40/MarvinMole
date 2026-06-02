cf65f3629868d334bd2301577e51aa2de88897a5
Morty40 <morten@perriard.dk>
2026-06-02 20:46:54 +0200

    cleanup

--- MarvinMole/Game/TileSet.swift
+++ MarvinMole/Game/TileSet.swift
@@ -8,26 +8,26 @@
 import SpriteKit
 
 class TileSet: SKTileSet {
-
+    
     let floors = SKTexture(imageNamed: "Floor").split().map {
         SKTileGroup(tileDefinition: SKTileDefinition(texture: $0)) }
-
+    
     let goal = SKTileGroup(imageNamed: "Goal")
-
+    
     let wallShadows = SKTexture(imageNamed: "WallShadows").split().map {
         SKTileGroup(tileDefinition: SKTileDefinition(texture: $0)) }
-
+    
     let shadows = SKTexture(imageNamed: "Shadows").split(columns: 3, rows: 3).map {
         SKTileGroup(tileDefinition: SKTileDefinition(texture: $0)) }
-
+    
     let outerWalls = SKTexture(imageNamed: "OuterWalls").split(columns: 3, rows: 7).map {
         SKTileGroup(tileDefinition: SKTileDefinition(texture: $0)) }
-
+    
     let innerWalls = SKTexture(imageNamed: "InnerWalls").split(columns: 3, rows: 5).map {
         SKTileGroup(tileDefinition: SKTileDefinition(texture: $0)) }
-
+    
     override init() {
-        super.init(tileGroups: [goal] + floors + wallShadows + shadows + outerWalls + innerWalls)
+        super.init(tileGroups: floors + [goal] + wallShadows + shadows + outerWalls + innerWalls)
     }
     
     required init?(coder: NSCoder) {
